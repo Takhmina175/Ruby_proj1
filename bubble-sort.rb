bafba6ca0f8230b68fb659f092a8e02357821ec1
mina <takhminam@gmail.com>
2020-10-30 17:45:06 +0300

    intsalling linters locally

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,21 +1,18 @@
 def bubble_sort_by(arr)
-    array_length = arr.length
-    arr if array_length <= 1
-   k = 1
-    while k < array_length-1 do
-      sorted = false
-  
-      (array_length-1).times do |i|
-        value = arr[i] <=> arr[i+1]
-        if value == 1
-          arr[i], arr[i+1] =  arr[i+1], arr[i]
-          sorted = true
-        end
+  array_length = arr.length - 1
+  arr if array_length <= 1
+  sorted = true
+  while sorted
+    sorted = false
+    array_length.times do |i|
+      value = yield arr[i], arr[i + 1]
+      if value.positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        sorted = true
       end
-      break if not sorted
-  
     end
-    arr
+    array_length -= 1
   end
-  sorted_arr = [2,56, 89, 0,78,1,5]
-  p bubble_sort_by(sorted_arr)
+  arr
+end
+p bubble_sort_by(%w[ruby css takhmina josias microverse]) { |left, right| left.length - right.length }
